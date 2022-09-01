05418b2ff3a7858ee83108bb72dd41f2b0d636a5
Alejandra Toro <torogrisalesa@gmail.com>
2022-09-01 11:23:27 -0500

    Ennumeración de los ejercicios

--- ejercicio2/main.java
+++ ejercicio2/main.java
@@ -3,26 +3,27 @@ import java.util.Scanner;
 /*
  * En este ejercicio practicarás las estructuras de control, para ello deberás crear:
 
-Usando un if, crear una condición que compare si la variable numeroIf es positivo, negativo, o 0.
+1.Usando un if, crear una condición que compare si la variable numeroIf es positivo, negativo, o 0.
 Pista: Los números inferiores a 0 son negativos y los superiores, positivos.
 
-Crea un bucle While, este bucle tendrá que tener como condición que la variable numeroWhile sea inferior a 3, el bloque de código que tendrá el bucle deberá:
+2-Crea un bucle While, este bucle tendrá que tener como condición que la variable numeroWhile sea inferior a 3, el bloque de código que tendrá el bucle deberá:
 
 Incrementar el valor de la variable en uno cada vez que se ejecute.
 
 Mostrarlo por pantalla cada vez que se ejecute.
 
-Para el bucle Do While, deberás crear la misma estructura que en el While, pero solo se debe ejecutar una vez.
+3.Para el bucle Do While, deberás crear la misma estructura que en el While, pero solo se debe ejecutar una vez.
 
-Para el bucle For, crea una variable numeroFor, esta variable tendrá como valor 0 y su condición será que la variable sea igual o menor que 3, se irá incrementando en 1 su valor cada vez que se ejecute y deberá mostrarse por pantalla.
+4.Para el bucle For, crea una variable numeroFor, esta variable tendrá como valor 0 y su condición será que la variable sea igual o menor que 3, se irá incrementando en 1 su valor cada vez que se ejecute y deberá mostrarse por pantalla.
 
-Por último, para el Switch, deberás crear la variable estacion, y distintos case para las cuatro estaciones del año. Dependiendo del valor de la variable estacion se deberá mandar un mensaje por consola informando de la estación en la que está. También habrá que poner un default para cuando el valor de la variable no sea una estación.
+5.Por último, para el Switch, deberás crear la variable estacion, y distintos case para las cuatro estaciones del año. Dependiendo del valor de la variable estacion se deberá mandar un mensaje por consola informando de la estación en la que está. También habrá que poner un default para cuando el valor de la variable no sea una estación.
  */
 
 public class main {
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
-        /*Integer numeroIf = Integer.parseInt(entrada.nextLine());
+        /*Ejercicio 1
+        Integer numeroIf = Integer.parseInt(entrada.nextLine());
         if(numeroIf>0){
             System.out.println("El número es positivo");
         }else if(numeroIf<0){
@@ -30,6 +31,8 @@ public class main {
         }else{
             System.out.println("El número es 0");
         }
+
+        Ejercicio 2
         Integer numeroWhile = Integer.parseInt(entrada.nextLine());
         while(numeroWhile>=3){
             numeroWhile = Integer.parseInt(entrada.nextLine());
@@ -39,15 +42,18 @@ public class main {
             numeroWhile++;
         }
 
+        Ejercicio 3
         do{
             System.out.println(numeroWhile);
            numeroWhile=3; 
 
         }while(numeroWhile<3);
 
+        Ejercicio 4
         for(int numeroFor =0;numeroFor<=3;numeroFor++){
             System.out.println(numeroFor);
         }*/
+        //Ejercicio 5
         String estacion = entrada.nextLine();
         switch(estacion){
             case "verano":
